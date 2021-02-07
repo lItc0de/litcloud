1e2910fa33903e99b13752de8113615431049535
litc0de <litc0de@posteo.org>
2021-02-07 22:23:40 +0100

    add file component

---
+++ frontend/src/components/File.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="file">
+    <img src="" alt="file" :id="fileId" />
+    <label :for="fileId">{{ fileName }}</label>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    fileId: String,
+    fileName: String,
+  },
+})
+export default class File extends Vue {
+  fileId!: string;
+
+  fileName!: string;
+}
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <File
+      v-for="{ fileId, fileName } in files"
+      :fileId="fileId"
+      :fileName="fileName"
+      :key="fileId"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import File from '@/components/File.vue';
 
 @Options({
   components: {
-    HelloWorld,
+    File,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  files: File[] = [
+    {
+      fileName: 'lol',
+      fileId: '1',
+    } as File,
+    {
+      fileName: 'lala',
+      fileId: '2',
+    } as File,
+  ];
+}
 </script>
